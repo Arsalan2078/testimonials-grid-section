34ba01e54740d08a1e710a0655a1cd414fdc5753
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-11 15:30:03 -0500

    grid layout complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import styles from "./App.module.css";
 import TestimonialsGrid from "./components/TestimonialsGrid";
 
 export default function App() {
   return (
-    <main data-text-preset="main">
+    <main className={styles.app} data-text-preset="main">
       <TestimonialsGrid />
     </main>
   );
